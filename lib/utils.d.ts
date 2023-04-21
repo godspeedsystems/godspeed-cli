daa145d0ac009904915b1f3bbed5dd05a867b04f
Ashutosh Tripathi <ashu17706@gmail.com>
2023-04-21 14:10:35 +0530

    added lib to .gitignore

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,4 +1,4 @@
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 export declare const prompt: promptSync.Prompt;
 export declare function ask(ques: string): boolean;
 export declare function userID(): string;
